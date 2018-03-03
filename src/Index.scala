b7b72d94377d96cace870b5a5243d527b5e4343b
JustinSchuyler <justin.r.schuyler@gmail.com>
2018-03-03 08:53:05 -0600

    tutorial(basics 2): apply, objects, match, case class, exceptions

--- src/Index.scala
+++ src/Index.scala
@@ -105,4 +105,127 @@ object Index extends App {
     def delete(key: K)
   }
   // def remove[K](key: K)
+
+  // apply = syntactic sugar for class/object with single use
+  class Canvas {}
+  object CanvasMaker {
+    def apply() = new Canvas
+  }
+  // with apply, we can call an object like a function
+  val newCanvas = CanvasMaker()
+  println(newCanvas)
+
+  // objects = single instances of a class (good for factories)
+  object Timer {
+    var count = 0
+
+    def currentCount(): Long = {
+      count += 1
+      count
+    }
+  }
+  println(Timer.currentCount())
+  println(Timer.currentCount())
+
+  // classes and objects can share the same name
+  // when they do, the object is a "companion object"
+  // companion objects are good for factories
+  class Flugzeug(brand: String)
+  object Flugzeug {
+    def apply(brand: String) = new Flugzeug(brand)
+  }
+  val airbus = Flugzeug("airbus")
+  val boeing = Flugzeug("boeing")
+  println(airbus)
+  println(boeing)
+
+  // functions are objects
+  // a function is a set of traits
+  // classes/objects can inherit from Function
+  // Function0 through Function22 exist
+  // the number indicates # of arguments
+  object addTwo extends Function1[Int, Int] {
+    def apply(m: Int): Int = m + 2
+  }
+  println(addTwo(4))
+  // shorthand
+  object addThree extends (Int => Int) {
+    def apply(m: Int): Int = m + 3
+  }
+
+  // packages
+  // package com.example.index
+  // values + functions must reside in class or object
+  // objects useful for holding static functions
+
+  // pattern matching
+  val dice = 2
+  dice match {
+    case 1 => "one"
+    case 2 => "two"
+    case _ => "some other number"
+  }
+  dice match {
+    case i if i == 1 => "one"
+    case i if i == 2 => "two"
+    case _ => "some other number"
+  }
+
+  // matching on type
+  def checker(x: Any): Any = {
+    x match {
+      case i: Int if i < 0 => i * -1
+      case d: Double => d + 0.1
+      case s: String => s + "."
+      case _ => "not sure"
+    }
+  }
+  println(checker(-2))
+  println(checker(2))
+  println(checker(2.2))
+  println(checker("hello"))
+
+  // case classes = conveniently store and match contents of class
+  case class Calculator(brand: String, model: String)
+  val a = Calculator("TI", "89")
+  val b = Calculator("TI", "89")
+  println(a == b)
+
+  def calcType(calc: Calculator): String = calc match {
+    case Calculator("TI", "89") => "school"
+    case Calculator("HP", "20B") => "financial"
+    case Calculator("HP", "48G") => "scientific"
+    // case Calculator(brand, model) => "Calculator: %s %s is of unknown type".format(brand, model)
+
+    // last match alternatives:
+    // case Calculator(_, _) => "unknown"
+    // case _ => "unknown"
+    // will print toString of c: Calculator(TI,9000)
+    //case c@Calculator(_, _) => "Calculator: %s of unknown type".format(c)
+    case c@Calculator(_, _) => "Calculator: %s %s of unknown type".format(c.brand, c.model)
+  }
+  println(calcType(a))
+  println(calcType(Calculator("HP", "48G")))
+  println(calcType(Calculator("TI", "9000")))
+
+  // exceptions
+  class ServerIsDownException extends Exception
+  class RemoteCalculator {
+    def add(m: Int, n: Int): Int = m + n
+    def addBroken(m: Int, n: Int): Int = throw new Exception
+    def addBroken2(m: Int, n: Int): Int = throw new ServerIsDownException
+    // convention: include parentheses if method has side effects
+    def close(): Unit = println("close remote calculator")
+  }
+
+  val remote = new RemoteCalculator
+  try {
+    println(remote.add(1, 2))
+    println(remote.addBroken2(2, 3))
+  } catch {
+    case e: ServerIsDownException => println("error: remote calculator server is down")
+    case e: Exception => println("error: log remote calculator exception")
+  } finally {
+    remote.close()
+  }
 }
