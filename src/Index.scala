a3bd07289788ca5b784f535dc8008f9fa3174358
JustinSchuyler <justin.r.schuyler@gmail.com>
2018-03-03 12:16:12 -0600

    tutorial(collections): data structures, functional combinators

--- src/Index.scala
+++ src/Index.scala
@@ -214,7 +214,7 @@ object Index extends App {
     def add(m: Int, n: Int): Int = m + n
     def addBroken(m: Int, n: Int): Int = throw new Exception
     def addBroken2(m: Int, n: Int): Int = throw new ServerIsDownException
-    // convention: include parentheses if method has side effects
+    // convention: include parentheses if method has side-effects
     def close(): Unit = println("close remote calculator")
   }
 
@@ -228,4 +228,130 @@ object Index extends App {
   } finally {
     remote.close()
   }
+
+  // arrays preserve order, can contain duplicates, are mutable
+  val numbers = Array(1, 2, 1)
+  // prints something like [I@5315b42e
+  println(numbers)
+  println(numbers(1))
+  numbers(1) = 5
+  println(numbers(1))
+
+  // lists preserve order, can contain duplicates, are immutable
+  val numbers2 = List(1, 2, 1)
+  // "value update is not a member of List[Int]"
+  // numbers2(1) = 7
+
+  // sets do not preserve order, no duplicates
+  val numbers3 = Set(1, 2, 1)
+  println(numbers3)
+
+  // tuple groups simple logical collections without using a class
+  val hostPort = ("localhost", 80)
+  // tuples don't have named accessors; instead they are named by position (1-based)
+  println("host: " + hostPort._1)
+  println("port: " + hostPort._2)
+
+  val matchMessage = hostPort match {
+    case ("localhost", 80) => "perfect match!"
+    case ("localhost", port) => "host matched; port: %d".format(port)
+    case (host, port) => "host: %s; port: %d".format(host, port)
+  }
+  println(matchMessage)
+
+  // syntactic sugar for creating tuple of 2 values
+  val sugarTuple = 1 -> 2
+  println(sugarTuple)
+
+  // maps can hold basic datatypes
+  val testMap = Map("holyGrail" -> "over the bridge and through the woods")
+  println(testMap)
+  println(testMap("holyGrail"))
+
+  // Option is a container that may or may not hold a value
+  val numberMap = Map("one" -> 1, "two" -> 2)
+  val optionFromMap = numberMap.get("one")
+  // prints Some(1)
+  println(optionFromMap)
+  val result = optionFromMap.getOrElse(0)
+  println(result * 7)
+
+  val optionFromMap2 = numberMap.get("three")
+  // prints None
+  println(optionFromMap2)
+  val result2 = optionFromMap2.getOrElse(0)
+  println(result * 9)
+
+  // pattern matching works with options too
+  val result3 = optionFromMap match {
+    case Some(n) => n * 2
+    case None => 0
+  }
+  println(result3)
+
+  // map is a functional combinator
+  def squared(m: Double): Double = scala.math.pow(m, 2)
+  val squarables = List(1.0, 5.0, 9.0)
+  // val squaredResults = squarables.map((m: Double) => squared(m))
+  val squaredResults = squarables.map(squared)
+  println(squaredResults)
+
+  // foreach is like map, but returns nothing (intended for side-effects only)
+  squarables.foreach((m: Double) => m * 2)
+
+  // filter removes elements where the function passed in evaluates to false
+  // functions that return booleans = predicate functions
+  val largeNumbers = squarables.filter((m: Double) => m > 5)
+  println(largeNumbers)
+  def isEven(m: Int): Boolean = m % 2 == 0
+  println(List(2, 5, 17, 24).filter(isEven))
+
+  // zip aggregates two lists into single list of pairs
+  val pairs = List(1, 2, 3).zip(List("a", "b", "c"))
+  println(pairs)
+
+  // partition splits a list based on a predicate function
+  val run = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+  val runPartitioned = run.partition(_ % 2 == 0)
+  println(runPartitioned)
+
+  // find returns the first element that matches the predicate
+  val target = run.find(_ > 5)
+  // target is an Option[Int]
+  println(target)
+
+  // drop removes the first i elements
+  println(run.drop(5))
+
+  // dropWhile removes elements matching the predicate until the predicate fails to match
+  // 9 will survive because it's shielded by 8
+  println(List(1, 3, 5, 8, 9).dropWhile(_ % 2 != 0))
+
+  // foldLeft takes a starting value, iterates over collection (left to right), adds to accumulator
+  val compressed = run.foldLeft(0)((acc: Int, cur: Int) => acc + cur)
+  println(compressed)
+  // see each iteration:
+  val compressed2 = run.foldLeft(0) { (acc: Int, cur: Int) => println("acc: " + acc + " cur: " + cur); acc + cur }
+  println(compressed2)
+
+  // foldRight does the same, iterating right to left
+
+  // flatten collapses one level of a nested structure
+  println(List(List(1, 2), List(3, 4)).flatten)
+
+  // flatMap will map and flatten
+  val nestedNumbers = List(List(1, 2), List(3, 4))
+  val flatMapResult = nestedNumbers.flatMap(x => x.map(_ * 2))
+  println(flatMapResult)
+  // short-hand for:
+  val flatMapResult2 = nestedNumbers.map((x: List[Int]) => x.map(_ * 2)).flatten
+  println(flatMapResult2)
+
+  // Maps + functional combinators
+  val phoneExtensions = Map("Sally" -> 100, "Marly" -> 101, "Olive" -> 201)
+  // this requires positional accessors:
+  val filteredPhones = phoneExtensions.filter((namePhone: (String, Int)) => namePhone._2 < 200)
+  // we can get around that with case:
+  val filteredPhones2 = phoneExtensions.filter({ case (name, extension) => extension < 200 })
+  println(filteredPhones2)
 }
